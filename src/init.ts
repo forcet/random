c8fbc4ac3efe7e0fa1b4dfbb575ae2f9c3546523
Mario Correa <mario.correa@fisagrp.com>
2025-10-27 18:31:27 -0500

    fix: implement get for service

--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response, NextFunction } from "express";
 import luckyRouter from "./routes/Lucky";
+import health from "./routes/Health";
+import start from "./routes/Start";
+import readiness from "./routes/Readiness";
 
 const app = express();
 app.use(express.json());
 app.use(luckyRouter);
+app.use(health);
+app.use(start);
+app.use(readiness);
 
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err);
