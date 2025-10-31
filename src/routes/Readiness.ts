46070ebe4d13fcf2e951e87cd7901a53f7e89149
Mario Correa <mario.correa@fisagrp.com>
2025-10-31 15:33:27 -0500

    fix: update service

--- src/routes/Readiness.ts
+++ src/routes/Readiness.ts
@@ -4,6 +4,6 @@ const router = Router();
 
 router.get('/readiness', (_req, res) => {
     res.sendStatus(200);
-  });
+});
 
 export default router;
--- src/routes/Start.ts
+++ src/routes/Start.ts
@@ -4,6 +4,6 @@ const router = Router();
 
 router.get('/startup', (_req, res) => {
     res.sendStatus(200);
-  });
+});
 
 export default router;
